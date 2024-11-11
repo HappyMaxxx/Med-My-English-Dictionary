5a9534f9ec9a70853c13c2db602f986a96844388
Max Patyk <mpatik2006@gmail.com>
2024-11-11 23:42:08 +0200

    v 0.1.2 Word model wa added, start setting up the admin panel

--- med/admin.py
+++ med/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('word', 'translation', 'time_create', 'time_update')
+    list_display_links = ('word', 'translation')
+    list_filter = ('word', 'time_create')
+    search_fields = ('word', 'translation')
+
+admin.site.register(Word, WordAdmin)
--- med/models.py
+++ med/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length=100)
+    translation = models.CharField(max_length=100)
+    example = models.TextField(max_length=1000, blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.word
+    
+    class Meta:
+        ordering = ['-time_create', 'word']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
