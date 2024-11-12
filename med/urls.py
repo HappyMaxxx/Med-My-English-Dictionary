0adb65b9d380437d7fa84ac3e6b297ad66eb3d64
Max Patyk <mpatik2006@gmail.com>
2024-11-12 17:53:07 +0200

    v 0.1.5.1 first version of words list

--- med/urls.py
+++ med/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('addword/', views.addword, name='addword'),
+    path('words/', views.WordListView.as_view(), name='words'),
 ]
--- med/views.py
+++ med/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import redirect, render
+from django.views.generic import ListView
 
 from med.forms import AddWordForm
 from med.models import *
@@ -15,12 +16,17 @@ def addword(request):
         form = AddWordForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('words')
 
     else:
         form = AddWordForm()
 
     return render(request, 'med/addword.html', {'title': 'Add New Word', 'form': form})
+
+
+class WordListView(ListView):
+    model = Word
+    template_name = 'med/words.html'
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>404 Page Not Found</h1>")
