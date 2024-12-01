84c117f89db3595fd5dbd3135bcd103e89cb73d4
Max <mpatik2006@gmail.com>
2024-12-01 20:55:20 +0200

    v 0.1.8.2 patch 4.1 admin friendship

--- med/admin.py
+++ med/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import *
+
 
 class WordAdmin(admin.ModelAdmin):
     list_display = ('word', 'translation', 'time_create', 'time_update')
@@ -9,10 +9,18 @@ class WordAdmin(admin.ModelAdmin):
     list_filter = ('word', 'time_create')
     search_fields = ('word', 'translation')
 
+
 class WordGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'user', 'is_main')
     list_filter = ('name',)
     search_fields = ('name',)
 
+
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ('status', 'sender', 'receiver')
+    list_filter = ('status',)
+    search_fields = ('sender', 'receiver',)
+
 admin.site.register(Word, WordAdmin)
 admin.site.register(WordGroup, WordGroupAdmin)
+admin.site.register(Friendship, FriendshipAdmin)
