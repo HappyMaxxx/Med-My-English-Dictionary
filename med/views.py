cc0988051560a8f090d30c24c33888bd5926cb5f
v1mer <mpatik2006@gmail.com>
2024-12-08 13:39:26 +0200

    v 0.1.8.3 added a burger menu for phones

--- med/views.py
+++ med/views.py
@@ -105,9 +105,7 @@ class ConfirmDeleteView(View):
     def post(self, request, *args, **kwargs):
         word_ids = request.POST.getlist('word_ids')
         group_id = request.POST.get('group_id')
-
-        print("POST Data:", word_ids, group_id)
-
+        
         if group_id and not word_ids:
             group = get_object_or_404(WordGroup, id=group_id, user=request.user)
             group.delete()
